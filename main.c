1497e18bbe7e6be52559d01c38c73e567b0495df
Eric Mesquita <ericmesquita2480@alu.ufc.br>
2023-09-13 09:27:29 -0300

    mod main.c

--- main.c
+++ main.c
@@ -16,16 +16,6 @@ void Task1(void *pvParameters) {
     }
 }
 
-// Function that represents the second task
-void StopTask(void *pvParameters) {
-    for (;;)
-    {
-        printf("Ending RTOS kernel...\n");
-        // vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 0.5 seconds
-        vTaskDelete(Task2_Handle);
-        vTaskEndScheduler();
-    }
-}
 
 // Function that represents the second task
 void Task2(void *pvParameters) {
@@ -34,37 +24,39 @@ void Task2(void *pvParameters) {
         printf("Task 2 is running...\n");
         vTaskDelete(Task1_Handle);
         vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 2 seconds
-        xTaskCreate(StopTask, "StopTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     }
 }
 
-/ Function that represents the first task
-void Task1(void *pvParameters) {
-    for (;;)
-    {
-        printf("Task 3 is running...\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 3 second
-    }
-}
 
-// Function that represents the second task
+// Function that ends the task 3
 void StopTask(void *pvParameters) {
     for (;;)
     {
         printf("Ending RTOS kernel...\n");
         // vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 0.5 seconds
-        vTaskDelete(Task2_Handle);
+        vTaskDelete(Task3_Handle);
         vTaskEndScheduler();
     }
 }
 
+
+// Function that represents the first task
+void Task3(void *pvParameters) {
+    for (;;)
+    {
+        printf("Task 3 is running...\n");
+        vTaskDelete(Task2_Handle);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 3 second
+        xTaskCreate(StopTask, "StopTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    }
+}
 
 
 int main(void) {
     // Initialize the FreeRTOS kernel
     xTaskCreate(Task1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, &Task1_Handle);
     xTaskCreate(Task2, "Task2", configMINIMAL_STACK_SIZE, NULL, 1, &Task2_Handle);
-    xTaskCreate(Task3, "Task3", configMINIMAL_STACK_SIZE, NULL, 3, &Task3_Handle);
+    xTaskCreate(Task3, "Task3", configMINIMAL_STACK_SIZE, NULL, 1, &Task3_Handle);
 
     // Start the FreeRTOS scheduler
     vTaskStartScheduler();
